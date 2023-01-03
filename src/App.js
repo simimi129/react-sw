a7ed3e0e0007dba349a5acd8ecc1eeceac00a654
simimi129 <simimi129@gmail.com>
2023-01-03 11:34:16 +0100

    transforming promise to async/await

--- src/App.js
+++ src/App.js
@@ -6,22 +6,18 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoveisHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const formattedMovies = data.results.map((movie) => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            openingText: movie.opening_crawl,
-            releaseDate: movie.release_date,
-          };
-        });
-        setMovies(formattedMovies);
-      });
+  async function fetchMoveisHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    const formattedMovies = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      };
+    });
+    setMovies(formattedMovies);
   }
 
   return (
